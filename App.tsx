5939dedf19987e92f0fd20b0373dac91501275ba
nVietUK <83152264+nVietUK@users.noreply.github.com>
2023-02-19 09:16:03 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -4,8 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see sme magic happen :)</p>
+      <h1>Hello World!</h1>
     </div>
   );
 }
