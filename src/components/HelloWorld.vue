a7cc06ccd34aa02ad483bc2683da89a28f72f8c9
nVietUK <vietuk.nguyen.git@icloud.com>
2023-02-20 06:03:04 +0700

    update 5 files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const count = ref(0)
   <p>
     Install
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
+    in your IDE for abetter DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,6 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import '../css/style.css'
 import App from '../vue/App.vue'
-import VueAspectRatio from 'vue-aspect-ratio'
-
-// vue-aspect-ratio
-Vue.defineComponent("vue-aspect-ratio", VueAspectRatio)
 
 // Vuetify
 import 'vuetify/styles'
@@ -14,4 +10,4 @@ import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({ components, directives, })
 
-Vue.createApp(App).use(vuetify).mount('#app')
+createApp(App).use(vuetify).mount('#app')
