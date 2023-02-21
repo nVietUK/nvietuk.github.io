5f5b148896646493d1b43e38ecef9079105d16f8
nVietUK <vietuk.nguyen.git@icloud.com>
2023-02-21 21:26:31 +0700

    ?

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,17 +1,43 @@
 <script lang="ts">
+import { computed, onMounted, onUnmounted, ref } from "vue"
 
+export const windowsWidthHeight=() => {
+  let windowWidth=ref(window.innerWidth)
+  let windowHeight=ref(window.innerHeight)
+
+  const onWidthChange=() => windowWidth.value=window.innerWidth
+  const onHeightChange=() => windowHeight.value=window.innerHeight
+  onMounted(() => {
+    window.addEventListener('resize', onWidthChange)
+    window.addEventListener('resize', onHeightChange)
+  })
+  onUnmounted(() => {
+    window.removeEventListener('resize', onWidthChange)
+    window.removeEventListener('resize', onHeightChange)
+  })
+  
+  const width = computed(() => windowWidth.value)
+  const height = computed(() => windowHeight.value)
+
+  return { width, height }
+}
 </script>
 
 <template>
   <div>
-    <v-card>
-      <v-responsive :aspect-ratio="4/3">
-        <v-card-text>
-          This card will always be 16:9 (unless you put more stuff in it)
-        </v-card-text>
-      </v-responsive>
-    </v-card>
+    <div class="screen">
+
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.screen {
+  aspect-ratio: 4/3;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  outline: 2px solid white;
+}
+</style>
