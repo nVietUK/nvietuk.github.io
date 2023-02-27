0765c622831a59b195669024db17e7f1ff4cc155
nVietUK <vietuk.nguyen.git@icloud.com>
2023-02-28 05:48:05 +0700

    not found

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,6 +4,7 @@ import main from '@/vue/main.vue'
 import routerweb from '@/vue/router.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import home from '@/vue/Home.vue'
+import notfoundp from '@/vue/notfound.vue'
 
 // Vuetify
 import 'vuetify/styles'
@@ -12,20 +13,21 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
-  components,
-  directives,
+	components,
+	directives,
 })
 
 // Vue router
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {path: '/home', name: 'home', component: home},
-        {path: '/', name: 'router', component: main}
-    ]
+	history: createWebHistory(),
+	routes: [
+		{ path: '/home', name: 'home', component: home },
+		{ path: '/', name: 'router', component: main },
+		{ path: '/:pathMatch(.*)*', component: notfoundp },
+	]
 })
 
 createApp(routerweb)
-.use(router)
-.use(vuetify)
-.mount('#router')
+	.use(router)
+	.use(vuetify)
+	.mount('#router')
---
+++ src/vue/notfound.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="parent404" style="
+                      -moz-user-select: none;
+                      -webkit-user-select: none;
+                      -ms-user-select: none;
+                      user-select: none;
+                      -o-user-select: none;
+                    " unselectable="on" onselectstart="return false;" onmousedown="return false;" id="myText">
+        <p style="font-size: 50vh; color:black; font-family: handwriting;" id="subText">
+            <RouterLink style="text-decoration: none; color: inherit;" to="/">?</RouterLink>
+        </p>
+    </div>
+</template>
+
+<style>
+.parent404 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  filter: invert(1);
+  mix-blend-mode: difference;
+}
+</style>
